9580c6172b60d3606763f8f5464a385c9734abed
Rhiju Das <rhiju@stanford.edu>
2017-05-30 10:15:43 -0700

    m2seq_summary() script, wrapper around Biers stuff

--- matlab/m2seq_summary.m
+++ matlab/m2seq_summary.m
@@ -1,18 +1,35 @@
-function m2seq_summary( r, summary_title );
+function [r,Z] = m2seq_summary( r, summary_title );
 % m2seq_summary( r, summary_title );
 %
 %  useful plots for m2seq data:
 %
-% raw data      ref. secondary structure with 1D
+% 1. raw data      
+% 2. ref. secondary structure with 1D
+% 3. Z-score      
+% 4. Z-score with reference bps
 %
-% Z-score       Z-score with reference bps
+% r             = [REQUIRED] RDAT filename or RDAT object. (can also be a cell of {r, r_nomod})
+% summary_title = [OPTION] title for first plot (default: RDAT filename or name)
 %
-%
-%
+% (C) Rhiju Das, Stanford University, 2017
 
+if ~exist( 'r', 'var' ) help( mfilename); return; end;
+if ~exist( 'varna_fig', 'file' ); 
+     fprintf( 'Please set up VARNA output via Biers: https://ribokit.github.io/Biers/\n' );
+     return;
+end
+if iscell( r )
+    r_nomod = r{2};
+    r = r{1};
+end
 if ~isobject( r ) & ischar( r )
     r_name = r;
     r = read_rdat_file( r_name );
+end
+if ~exist( r_nomod ) 
+    r_nomod = [];
+elseif ~isobject( r_nomod ) & ischar( r_nomod )
+    r_nomod = read_rdat_file( r_nomod );
 end
 if ~exist( 'summary_title', 'var' )
     if exist( 'r_name', 'var' ) summary_title = r_name; else; r_name = r.name; end
@@ -25,12 +42,15 @@ output_varna( '/tmp/temp.png', r.sequence, r.structure,[],[],r.offset,[],[],SHAP
 imshow( imread( '/tmp/temp.png' ) );
 
 subplot( 2, 2, 3);
-Z = output_Zscore_from_rdat( [], r );
+Z = output_Zscore_from_rdat( [], r, r_nomod, [], 1, 1 );
 rZ = get_Zscore_rdat( r, Z );
 scalefactor = [];
 m2seqplot( rZ, [], scalefactor );
+
 subplot( 2, 2, 4);
-m2seqplot( rZ, [], scalefactor, [],  1 );
+%m2seqplot( rZ, [], scalefactor, [],  1 );
+print_mode = 0;
+cluster_z_scores( Z, r.structure, r.offset, print_mode );
 
 subplot(2,2,1);
 title( summary_title );
